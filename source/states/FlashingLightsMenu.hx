388a886cdf14c577c139cd2fd40fa8af9b9ed86c
Raiden Alfares 100% REAL <97826418+RaidenAlfares12@users.noreply.github.com>
2022-04-19 11:45:28 +0700

    Initial commit

---
+++ source/states/FlashingLightsMenu.hx
@@ -0,0 +1,37 @@
+package states;
+
+import utilities.Options;
+import flixel.FlxG;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+
+class FlashingLightsMenu extends MusicBeatState
+{
+    override public function create()
+    {
+        super.create();
+
+        var text = new FlxText(0,0,0,"Hey! Leather Engine has flashing lights\nPress A to enable them, or B to not.\n(Any key closes this menu)", 32);
+        text.font = Paths.font("vcr.ttf");
+        text.screenCenter();
+        text.setBorderStyle(OUTLINE, FlxColor.BLACK, 1.5, 1);
+        add(text);
+
+        #if android
+        addVirtualPad(NONE, A_B);
+        #end
+    }
+
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+        if(controls.ACCEPT)
+            Options.setData(true, "flashingLights");
+        else if(!controls.ACCEPT && controls.BACK)
+            Options.setData(false, "flashingLights");
+
+        if(controls.BACK)
+            FlxG.switchState(new TitleState());
+    }
+}
---
+++ source/states/MusicBeatState.hx
@@ -0,0 +1,185 @@
+package states;
+
+import flixel.input.FlxInput.FlxInputState;
+import flixel.FlxSprite;
+import flixel.FlxBasic;
+import openfl.Lib;
+import lime.app.Application;
+import game.Conductor;
+import utilities.PlayerSettings;
+import game.Conductor.BPMChangeEvent;
+import utilities.Controls;
+import flixel.FlxG;
+import flixel.addons.ui.FlxUIState;
+#if android
+import flixel.input.actions.FlxActionInput;
+import android.AndroidControls.AndroidControls;
+import android.FlxVirtualPad;
+#end
+
+class MusicBeatState extends FlxUIState
+{
+	public var lastBeat:Float = 0;
+	public var lastStep:Float = 0;
+
+	public var curStep:Int = 0;
+	public var curBeat:Int = 0;
+	private var controls(get, never):Controls;
+
+	public static var windowNameSuffix:String = "";
+	public static var windowNamePrefix:String = "Leather Engine";
+
+	public static var fullscreenBind:String = "F11";
+
+	inline function get_controls():Controls
+		return PlayerSettings.player1.controls;
+
+	#if android
+	var _virtualpad:FlxVirtualPad;
+	var androidc:AndroidControls;
+	var trackedinputs:Array<FlxActionInput> = [];
+	#end
+	
+	#if android
+	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
+		_virtualpad = new FlxVirtualPad(DPad, Action);
+		_virtualpad.alpha = 0.75;
+		add(_virtualpad);
+		controls.setVirtualPad(_virtualpad, DPad, Action);
+		trackedinputs = controls.trackedinputs;
+		controls.trackedinputs = [];
+	}
+	#end
+
+	#if android
+	public function addAndroidControls() {
+                androidc = new AndroidControls();
+
+		switch (androidc.mode)
+		{
+			case VIRTUALPAD_RIGHT | VIRTUALPAD_LEFT | VIRTUALPAD_CUSTOM:
+				controls.setVirtualPad(androidc.vpad, FULL, NONE);
+			case DUO:
+				controls.setVirtualPad(androidc.vpad, DUO, NONE);
+			case HITBOX:
+				controls.setHitBox(androidc.hbox);
+			default:
+		}
+
+		trackedinputs = controls.trackedinputs;
+		controls.trackedinputs = [];
+
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		androidc.cameras = [camcontrol];
+
+		androidc.visible = false;
+
+		add(androidc);
+	}
+	#end
+
+	#if android
+        public function addPadCamera() {
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];
+	}
+	#end
+	
+	override function destroy() {
+		#if android
+		controls.removeFlxInput(trackedinputs);
+		#end	
+		
+		super.destroy();
+	}
+	
+	override function update(elapsed:Float)
+	{
+		//everyStep();
+		var oldStep:Int = curStep;
+
+		updateCurStep();
+		updateBeat();
+
+		if (oldStep != curStep && curStep > 0)
+			stepHit();
+
+		super.update(elapsed);
+
+		if(FlxG.stage != null)
+			FlxG.stage.frameRate = utilities.Options.getData("maxFPS");
+
+		if(!utilities.Options.getData("antialiasing"))
+		{
+			forEachAlive(function(basic:FlxBasic) {
+				if(Std.isOfType(basic, FlxSprite))
+					Reflect.setProperty(basic, "antialiasing", false);
+			}, true);
+		}
+
+		if(FlxG.keys.checkStatus(FlxKey.fromString(utilities.Options.getData("fullscreenBind", "binds")), FlxInputState.JUST_PRESSED))
+			FlxG.fullscreen = !FlxG.fullscreen;
+
+		Application.current.window.title = windowNamePrefix + windowNameSuffix;
+	}
+
+	private function updateBeat():Void
+	{
+		curBeat = Math.floor(curStep / Conductor.timeScale[1]);
+	}
+
+	private function updateCurStep():Void
+	{
+		var lastChange:BPMChangeEvent = {
+			stepTime: 0,
+			songTime: 0,
+			bpm: 0
+		}
+		
+		for(i in 0...Conductor.bpmChangeMap.length)
+		{
+			if (Conductor.songPosition >= Conductor.bpmChangeMap[i].songTime)
+				lastChange = Conductor.bpmChangeMap[i];
+		}
+
+		var dumb:TimeScaleChangeEvent = {
+			stepTime: 0,
+			songTime: 0,
+			timeScale: [4,4]
+		};
+
+		var lastTimeChange:TimeScaleChangeEvent = dumb;
+
+		for(i in 0...Conductor.timeScaleChangeMap.length)
+		{
+			if (Conductor.songPosition >= Conductor.timeScaleChangeMap[i].songTime)
+				lastTimeChange = Conductor.timeScaleChangeMap[i];
+		}
+
+		if(lastTimeChange != dumb)
+			Conductor.timeScale = lastTimeChange.timeScale;
+
+		var multi:Float = 1;
+
+		if(FlxG.state == PlayState.instance)
+			multi = PlayState.songMultiplier;
+
+		Conductor.recalculateStuff(multi);
+
+		curStep = lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
+
+		updateBeat();
+	}
+
+	public function stepHit():Void
+	{
+		if (curStep % Conductor.timeScale[0] == 0)
+			beatHit();
+	}
+
+	public function beatHit():Void { /* do literally nothing dumbass */ }
+}
